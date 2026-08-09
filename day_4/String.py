2a4ec24978cdb2c1ca743e20e451358f994642e9
Mahesh kumar <vickymandal542@gmail.com>
2026-08-09 23:55:49 +0530

    string concat

---
+++ day_4/String.py
@@ -0,0 +1,7 @@
+# Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+s1 = "Thirty"
+s2 = "Days"
+s3 = "Of"
+s4 = "Python"
+result = s1 + " " + s2 + " " + s3 + " " + s4
+print(result)
