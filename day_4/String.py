09339e4597a002a172394e309748bd5bcb1b715e
Mahesh kumar <vickymandal542@gmail.com>
2026-08-10 23:48:38 +0530

    string methods

--- day_4/String.py
+++ day_4/String.py
@@ -6,3 +6,12 @@ s4 = "Python"
 result = s1 + " " + s2 + " " + s3 + " " + s4
 print(result)
 
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+s1 = "Coding"
+s2 = "For"
+s3 = "All"
+sentence = f"{s1} {s2} {s3}"
+print(sentence)
+#Declare a variable named company and assign it to an initial value "Coding For All".
+
+#Print the variable company using print().
